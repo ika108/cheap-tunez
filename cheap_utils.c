2044f387bade703a153f837bf1ff48e089cf79b9
Sven SZEJWACH <git@nimporteou.net>
2011-08-16 19:12:15 +0200

    Cleaning files names Adding header files Cleaning code an comments

--- cheap_utils.c
+++ cheap_utils.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <unistd.h>
@@ -9,41 +9,43 @@
 #include "cheap_utils.h"
 
 int find_pid_by_name(const char *name) {
-	DIR *od=opendir("/proc");
-	int fd=0;
+	DIR *od = opendir("/proc"); // Create a FD for /proc directory
+	int fd = 0;
 	char fname[256*2];
-	char buf[256*2]="\0";
+	char buf[256*2] = "\0";
 	struct dirent *de;
 	size_t nbread;
-	int pid=-1;
-	if(!od) return -1;
-	while ((de = readdir(od)) != NULL)
-	{
-		snprintf(fname, sizeof(fname), "/proc/%s/cmdline", de->d_name);
-		fd=open(fname, O_RDONLY);
-		if(fd==-1) continue;
-		nbread=read(fd, buf, sizeof(buf));
-		if(nbread==-1)  {
+	int pid = -1;
+	if(!od) return -1; // Couldn't open /proc
+	while ((de = readdir(od)) != NULL) { // Iteration over /proc content
+		/* This trick can work only because readdir iterate on an alphanumeric order. Making the pid numbers first.
+		Maybe we should verify that de->d_name is a number before processing it */
+		snprintf(fname, sizeof(fname), "/proc/%s/cmdline", de->d_name); // Store next cmdline content into fname
+		fd = open(fname, O_RDONLY);
+		if(fd == -1) continue; // If we fail to open fname, (and we will) just skip the entry.
+		nbread = read(fd, buf, sizeof(buf));
+		if(nbread == -1) {
 			close(fd);
 			continue;
 		}
 		if(strstr(buf, name)) {
-			//now, check that the executable name ends with the name we search. This will prevent grabbing the PID of vi, fro instance, if we have a "vi midi_input.c"
+			/* now, check that the executable name ends with the name we search. 
+			   This will prevent grabbing the PID of vi, fro instance, if we have a "vi midi_input.c" */
 
 			snprintf(fname, sizeof(fname), "/proc/%s/exe", de->d_name);
-			nbread=readlink(fname, buf, sizeof(buf));
-			if(nbread==-1) {
+			nbread = readlink(fname, buf, sizeof(buf));
+			if(nbread == -1) {
 				close(fd);
 				continue;
 			}
-			buf[nbread]='\0';
+			buf[nbread] = '\0';
 			if(strlen(buf)>strlen(name)) {
-				if(strcmp(buf+strlen(buf)-strlen(name), name)==0) {
-					pid=atoi(de->d_name);
+				if(strcmp(buf + strlen(buf) - strlen(name), name) == 0) {
+					pid = atoi(de->d_name);
 					break;
 				}
 
-}
+			}
 		}
 	}
 
